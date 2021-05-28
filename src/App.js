eb902adbd1f1a43438e675b6e52af2f0dec140ab
Wei Luo <alexcluoo@gmail.com>
2021-05-28 01:45:21 -0500

    Update menu-items to navigate withRoute HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import React from "react";
+import { Route, Switch } from "react-router";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+const TopicsList = () => (
+  <div>
+    <h1>TOPTICS LIST PAGE</h1>
+  </div>
+);
+
+const TopicDetails = (props) => (
+  <div>
+    <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/topics" component={TopicsList} />
+        <Route path="/topics/:topicId" component={TopicDetails} />
+      </Switch>
     </div>
   );
 }
