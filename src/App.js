dc9de459c01a976964d412498d004c460667c3c3
Kangeroo <64865570+kangeroooo@users.noreply.github.com>
2021-06-28 00:55:36 -0500

    Shop page (#4)
    * Add shop-page, collection-preview component and collection-item component
    
    * Add header component
    
    * Add sign-in form

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
-import React from "react";
-import { Route, Switch } from "react-router";
-import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
-
-const TopicsList = () => (
-  <div>
-    <h1>TOPTICS LIST PAGE</h1>
-  </div>
-);
-
-const TopicDetails = (props) => (
-  <div>
-    <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
-  </div>
-);
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import './App.css';
+import Header from './components/header/header.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
-        <Route exact path="/topics" component={TopicsList} />
-        <Route path="/topics/:topicId" component={TopicDetails} />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
