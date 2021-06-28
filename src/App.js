f74aca85575c10954df240b8abbd6b135102ecb7
Wei Luo <alexcluoo@gmail.com>
2021-06-28 00:54:14 -0500

    Add sign-in form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from "react";
-import { Route, Switch } from "react-router";
-import "./App.css";
-import Header from "./components/header/header.component";
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import './App.css';
+import Header from './components/header/header.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
