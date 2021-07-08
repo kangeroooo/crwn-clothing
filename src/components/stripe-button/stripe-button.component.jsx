dace0f147453852ae463dbbf2ef5d358b897cac0
Kangeroo <64865570+kangeroooo@users.noreply.github.com>
2021-07-08 01:09:50 -0500

    Updated stripe integration (#11)

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51JApwkLIDriTGt99YzGa236LAgGjvHXqssF93i6Yj3ibPJRu06fNp2MouJIr9rftUcljOCUeE3TQZQ4tw05EIj5600gFdaWhl1';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
