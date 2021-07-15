c7610ebd6bd22c8189a107096bb1c57e98db53ee
Kangeroo <64865570+kangeroooo@users.noreply.github.com>
2021-07-15 01:54:32 -0500

    Installed redux-thunk and created asycn action to load collections (#15)

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -3,9 +3,24 @@ import { createSelector } from 'reselect';
 
 const selectShop = (state) => state.shop;
 
+export const selectShopIsFetching = createSelector(
+  [selectShop],
+  (shop) => shop.isFetching
+);
+
+export const selectShopErrorMessage = createSelector(
+  [selectShop],
+  (shop) => shop.errorMessage
+);
+
 export const selectShopCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
+);
+
+export const selectIsShopCollectionsLoaded = createSelector(
+  [selectShopCollections],
+  (collections) => !!collections
 );
 
 export const selectShopCollectionsForPreview = createSelector(
